ed7a1a08922ec6f81acba75fb8c756f8d0980602
rncongi <rncongi@gmail.com>
2020-11-06 19:00:00 -0500

    Added the basics of creating a new character.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,16 +5,20 @@ class CharactersController < ApplicationController
   end
 
   post "/characters" do
-    
     #fix the params hash with class_lvl and remove the class_name and lvl_num
-
+    params[:class_lvl] = params[:class_name] + " " + params[:lvl_num]
+    params.delete("class_name")
+    params.delete("lvl_num")
     #create the character
-
+    @character = Character.create(params)
+    @character.user_id = current_user.id
+    @character.save
     #redirect to the new characters/:id page
-    redirect "/characters/#{character.id}"
+    redirect "/characters/#{@character.id}"
   end
 
   get "/characters/:id" do
+    @character = Character.find(params[:id])
     erb :"/characters/show"
   end
 
