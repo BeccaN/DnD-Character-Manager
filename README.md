2538c297da4e485ea316d9d07a7ce1542ec5c72b
rncongi <rncongi@gmail.com>
2020-11-12 18:10:35 -0500

    removed the seeds, schema and db file

--- README.md
+++ README.md
@@ -10,12 +10,16 @@ This Sinatra based CRUD/MVC model application allows users to track and manage t
     $ bundle install
 ```
 3. Make sure to provide a environment variable for the session secret in the ApplicationController
-4. Startup a shotgun host
+4. Run rake migrations to get the database started
+```console
+    $ rake db:migrate
+```
+5. Startup a shotgun host
 ```console
     $ shotgun
 ```
-4. Open server (localhost:9393)
-6. Create an account and start making characters!
+6. Open server (localhost:9393)
+7. Create an account and start making characters!
 
 ##Contributing Guide
 - Fork the repository and mess around!
--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-rebecca = User.create(name: 'Rebecca', email: 'bawkahemail@gmail.com', password: 'Blep1234@')
-zander = User.create(name: 'Zander', email: 'zandergamer@gmail.com', password: 'Test4567#')
-
-Character.create(name: 'Torgast', user_id: zander.id, race: 'Half Orc', class_lvl: 'Monk 4', alignment: 'NG', personality: 'Soft spoken and nervous. He believes in protecting the innocent.', photo: 'https://i.pinimg.com/originals/76/ce/c9/76cec992ffb19e86e9a5842ad1cf1348.jpg')
-Character.create(name: 'Ada', user_id: rebecca.id, race: 'Aasimar', class_lvl: 'Paladin 4', alignment: 'LG', personality: 'Usually found half in the bag. Very sarcastic and rude.', photo: 'https://i.pinimg.com/originals/4b/1e/33/4b1e33adb93bae5cdab280d9495af43c.jpg')
